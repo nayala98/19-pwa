c98c103b15e8aa5616640c661b7d3e56639fe42b
nayala98 <nathaniel.ayala20@gmail.com>
2021-08-16 16:38:05 -0500

    complete rework

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,78 +1,82 @@
+'use strict'
+
+console.log('This is your service-worker.js file!');
+
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/assets/css/styles.css",
-    "/assets/js/index.js",
-    "/assets/js/db.js",
-    "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-512x512.png",
-    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
-  ];
-  
-  const CACHE_NAME = "static-cache-v1";
-  const DATA_CACHE_NAME = "data-cache-v1";
-  
-  self.addEventListener("install", (evt) => {
-    evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {
-        return cache.addAll(FILES_TO_CACHE);
-      })
-    );
-  
-    self.skipWaiting();
-  });
-  
-  self.addEventListener("activate", (evt) => {
-    // remove old caches
-    evt.waitUntil(
-      caches.keys().then((keyList) => {
-        return Promise.all(
-          keyList.map((key) => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              return caches.delete(key);
-            }
-          })
-        );
-      })
-    );
-  
-    self.clients.claim();
-  });
-  
-  self.addEventListener("fetch", (evt) => {
-    // cache successful GET requests to the API
-    if (evt.request.url.includes("/api/") && evt.request.method === "GET") {
-      evt.respondWith(
+    `/db.js`,
+    `/index.html`,
+    `/index.js`,
+    `/index.css`,
+    `/manifest.webmanifest`,
+    `/img/icons/money.png`
+];
+
+const STATIC_CACHE = `static-cache-v1`;
+const RUNTIME_CACHE = `runtime-cache`;
+
+self.addEventListener(`install`, event => {
+    event.waitUntil(
         caches
-          .open(DATA_CACHE_NAME)
-          .then((cache) => {
-            return fetch(evt.request)
-              .then((response) => {
-                // If the response was good, clone it and store it in the cache.
-                if (response.status === 200) {
-                  cache.put(evt.request, response.clone());
-                }
-  
-                return response;
-              })
-              .catch(() => {
-                // Network request failed, try to get it from the cache.
-                return cache.match(evt.request);
-              });
-          })
-          .catch((err) => console.log(err))
-      );
-  
-      // stop execution of the fetch event callback
-      return;
-    }
-  
-    // if the request is not for the API, serve static assets using
-    // "offline-first" approach.
-    evt.respondWith(
-      caches.match(evt.request).then((response) => {
-        return response || fetch(evt.request);
-      })
+            .open(STATIC_CACHE)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
     );
-  });
+});
+
+self.addEventListener(`activate`, event => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    event.waitUntil(
+        caches
+            .keys()
+            .then(cacheNames =>
+                // return array of cache names that are old to delete
+                cacheNames.filter(cacheName => !currentCaches.includes(cacheName))
+            )
+            .then(cachesToDelete =>
+                Promise.all(
+                    cachesToDelete.map(cacheToDelete => caches.delete(cacheToDelete))
+                )
+            )
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener(`fetch`, event => {
+    if (
+        event.request.method !== `GET` ||
+        !event.request.url.startsWith(self.location.origin)
+    ) {
+        event.respondWith(fetch(event.request));
+        return;
+    }
+
+    if (event.request.url.includes(`/api/transaction`)) {
+        event.respondWith(
+            caches.open(RUNTIME_CACHE).then(cache =>
+                fetch(event.request)
+                    .then(response => {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    })
+                    .catch(() => caches.match(event.request))
+            )
+        );
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            return caches
+                .open(RUNTIME_CACHE)
+                .then(cache =>
+                    fetch(event.request).then(response =>
+                        cache.put(event.request, response.clone()).then(() => response)
+                    )
+                );
+        })
+    );
+});
--- server.js
+++ server.js
@@ -1,29 +1,25 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+'use strict';
 
-const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const express = require(`express`);
+const mongoose = require(`mongoose`);
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
-
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(`public`));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/budget`, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
 });
+
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require(`./routes/api.js`));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
